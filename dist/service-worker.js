00d88effbcb59dd26aa00eb187662d0bf8a0b2cf
luchao.ding <116710782@qq.com>
2019-06-15 15:49:05 +0800

    redux

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9eec79efadb25a0a6d0a14f4aadd2e53.js"
+  "/precache-manifest.6c6093604b21aa687857a58eb01a183a.js"
 );
 
 workbox.core.skipWaiting();
