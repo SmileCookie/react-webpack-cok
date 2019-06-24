f5d2855c7f0b01a9e02c6919a2153332975c6534
wangmeng <wangmeng@oswaldlink.com>
2019-06-24 10:41:17 +0800

    news

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,9 +5,11 @@
  */
 import React from 'react';
 import Logo from '../../assets/image/base/logo-btcwinex.png'
-import { Link,withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
+import {withRouter} from 'react-router'
 import { connect } from 'react-redux'
 import { FormattedMessage,injectIntl } from 'react-intl';
+import './header.less';
 
 //coinList: state.account.detail,
 @connect(
@@ -27,6 +29,7 @@ class Header extends React.Component {
            
         };
        this.jmp = this.jmp.bind(this)
+       this.ch = window.location.href;
     }
 
     
@@ -35,7 +38,7 @@ class Header extends React.Component {
     }
 
     jmp(url = ''){
-        url && (window.location.href = url);
+        
     }
 
     formatFundsDetail(result) {
@@ -69,9 +72,43 @@ class Header extends React.Component {
    
 
     render() {
+        const {formatMessage} = this.props.intl
+        const ch = this.ch;
         return (
             <header className="header up-mx-wp">
-                asdddad
+                <div className="contain clearfix">
+                    <div className="header-left">
+                        <a className="logo" href="/">
+                            <img  src={Logo} />
+                        </a>
+                        {/* <ul className="nav clearfix">
+                            <li>
+                                <Link title={formatMessage({id:"币币交易中心"})} onClick={()=>{this.jmp("/bw/trade/")}} className={ch.includes("/trade") ? 'bbyh-choosed' : ''} >
+                                    <FormattedMessage id="币币交易中心"/>
+                                </Link>
+                    
+                            </li>
+
+                            <li className="hot">
+                                <Link title={formatMessage({id:"多屏看板"})} onClick={()=>{this.jmp("/bw/multitrade")}}  className={ch.includes("/multitrade") ? 'bbyh-choosed' : ''} >
+                                    <FormattedMessage id="多屏看板"/>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link title={formatMessage({id:"公告"})} onClick={()=>{this.jmp("/bw/announcements")}}  className={ch.includes("/announcements") ? 'bbyh-choosed' : ''}>
+                                    <FormattedMessage id="公告"/>
+                                </Link>
+                            </li>
+
+                            <li>
+                            <Link title={formatMessage({id:"新闻"})} onClick={()=>{this.jmp("/bw/news")}}  className={ch.includes("/news") ? 'bbyh-choosed' : ''}>
+                                    <FormattedMessage id="新闻"/>
+                                </Link>
+                            </li>
+
+                        </ul> */}
+                    </div>
+                </div>
             </header>
         )
     }
